8c7328184f1dd24166edb9cfa4fb9771a531b533
Jens Tinfors <jens.tinfors@gmail.com>
2014-02-23 15:42:26 +0100

    crap

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    url = require('url'),
     instagram = require('./instagram'),
     // redis = require("redis"),
     // client = redis.createClient(), // TODO: handle error on create?
@@ -27,21 +28,19 @@ app.get('/subscriptions/', function(req, res) {
 });
 
 app.get('/subscriptions/callback/', function(req, res) {
-  console.log("IN CALLBACK! => ", req.query);
-  var hub_challange = req.query['hub.challange'];
-  var hub_token = req.query['hub.token'];
-  console.log(hub_challange);
-  if(hub_challange != null) {
+  console.log("GET /subscriptions/callback/ => ", req.query);
+  var parsedRequest = url.parse(req.url, true);
+  if('subscribe' === parsedRequest['query']['hub.mode'] && parsedRequest['query']['hub.challenge'] != null) {
     console.log("sending back hub_challange => ", hub_challange);
     res.send(hub_challange);
   } else {
-    console.log("sending back ok => ");
+    console.log("sending back nok => ");
     res.send(400, "nok");
   }
 });
 
 app.post('/subscriptions/callback/', function(req, res) {
-  console.log("RECIEVED UPDATE! => ", req);
+  console.log("POST /subscriptions/callback/ => ", req.body);
   res.send("ok");
 });
 
