25fd72be7e29bd865c0cf156e2a80205faf6c536
Jens Tinfors <jens.tinfors@gmail.com>
2014-03-04 22:20:47 +0100

    oops

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(function() {
   var ws = new WebSocket(host);
   ws.onmessage = function (event) {
     console.log(JSON.parse(event.data));
-    for(int i=0;i< event.data.length;i++) {
+    for(var i=0;i< event.data.length;i++) {
       $.ajax("https://api.instagram.com/v1/geographies/" + event.data[i].object_id + 
              "/media/recent?client_id=6d64c9abeec04916bc18caae41cfa396&count=1", function(data) {
                var li = document.createElement('li');
