b9145dcbb49f6eef67bbd7bc1fc18a435033162c
Jens Tinfors <jens.tinfors@gmail.com>
2014-03-04 22:23:50 +0100

    oops

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,10 @@ $(function() {
   var host = location.origin.replace(/^http/, 'ws')
   var ws = new WebSocket(host);
   ws.onmessage = function (event) {
-    console.log(JSON.parse(event.data));
-    for(var i=0;i< event.data.length;i++) {
-      $.ajax("https://api.instagram.com/v1/geographies/" + event.data[i].object_id + 
+    var parsed_data = JSON.parse(event.data);
+    console.log(parsed_data);
+    for(var i=0;i< parsed_data.length;i++) {
+      $.ajax("https://api.instagram.com/v1/geographies/" + parsed_data[i].object_id +
              "/media/recent?client_id=6d64c9abeec04916bc18caae41cfa396&count=1", function(data) {
                var li = document.createElement('li');
                li.innerHTML = data;
