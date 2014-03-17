ac4cf5fb6e65974bb8305b28ca26cd76cfeef5db
Jens Tinfors <jens.tinfors@gmail.com>
2014-03-17 11:49:06 +0100

    asdasd

--- public/js/main.js
+++ public/js/main.js
@@ -5,15 +5,9 @@ $(function() {
   ws.onmessage = function (event) {
     var parsed_data = JSON.parse(event.data);
     console.log(parsed_data);
-
-    // for(var i=0;i< parsed_data.length;i++) {
-    //   $.ajax("https://api.instagram.com/v1/geographies/" + parsed_data[i].object_id +
-    //          "/media/recent?client_id=6d64c9abeec04916bc18caae41cfa396&count=1", function(data) {
-    //            var li = document.createElement('li');
-    //            li.innerHTML = data;
-    //            document.querySelector('#pings').appendChild(li);
-    //          });
-    // }
+    $('<img/>', {
+      "src": parsed_data.data[0].images.low_resolution.url
+    }).appendTo($('<li>')).appendTo("#pings");
   };
 });
 
