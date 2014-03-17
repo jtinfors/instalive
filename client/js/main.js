90c1fa4d14e3b32393e357a2b17df5e6c8d2db9a
Jens Tinfors <jens.tinfors@gmail.com>
2014-03-17 21:56:27 +0100

    more trickery

--- client/js/main.js
+++ client/js/main.js
@@ -10,17 +10,18 @@ $(function() {
       for(var i=0; i < media.data.length;i++) {
         console.log(media.data[i]);
         var item = mustache.render("<li><div>\
-                        <a href=\"{{{link}}}\" class=\"thumbnail\">\
-                          <img title=\"{{{caption.text}}}\" src=\"{{{images.thumbnail.url}}}\" class=\"img-responsive img-rounded\"/>\
+                          <img title=\"{{{caption.text}}}\"\
+                                   src=\"{{{images.standard_resolution.url}}}\"\
+                                   class=\"img-responsive img-rounded\"\
+                                   height=\"{{images.standard_resolution.height}}\"\
+                                   width=\"{{images.standard_resolution.width}}\"/>\
                           <div class=\"caption\">\
                           <p>{{caption.text}}</p>\
                             {{#tags}}\
                               <span class=\"label label-default\">{{.}}</span>\
                             {{/tags}}\
                           </div>\
-                        </a>\
                         </div></li>", media.data[i]);
-        console.log(item);
         $(item).appendTo("#pings");
       }
     } else {
