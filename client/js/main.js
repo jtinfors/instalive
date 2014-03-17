dccf8d534d016e39564e0d0068cb067076260a11
Jens Tinfors <jens.tinfors@gmail.com>
2014-03-17 21:20:46 +0100

    more trickery

--- client/js/main.js
+++ client/js/main.js
@@ -9,7 +9,7 @@ $(function() {
     if(media.meta.code == 200 && media.data.length > 0) {
       for(var i=0; i < media.data.length;i++) {
         console.log(media.data[i]);
-        mustache.render("<li><div>\
+        var item = mustache.render("<li><div>\
                         <a href=\"{{{link}}}\" class=\"thumbnail\">\
                           <img title=\"{{{caption.text}}}\" src=\"{{{images.thumbnail.url}}}\" class=\"img-responsive img-rounded\"/>\
                           <div class=\"caption\">\
@@ -19,7 +19,9 @@ $(function() {
                             {{/tags}}\
                           </div>\
                         </a>\
-                        </div></li>", media.data[i]).appendTo("#pings");
+                        </div></li>", media.data[i]);
+        console.log(item);
+        item.appendTo("#pings");
       }
     } else {
       console.log("fail", event);
