d26f2416f90a63612460508916983907942cdab6
Jens Tinfors <jens.tinfors@gmail.com>
2014-03-17 16:41:48 +0100

    asdasd

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,32 @@ $(function() {
   var host = location.origin.replace(/^http/, 'ws')
   var ws = new WebSocket(host);
   ws.onmessage = function (event) {
-    var parsed_data = JSON.parse(event.data);
-    console.log(parsed_data);
-    $('<img/>', {
-      "src": parsed_data.data[0].images.low_resolution.url
-    }).appendTo($('<li>')).appendTo("#pings");
+    if(event.data.meta == 200) {
+      var media = JSON.parse(event.data);
+      console.log(media);
+      var text = "";
+      var tags = [];
+      var mentions = [];
+      for(var i=0; i < media.data.length;i++) {
+        if(media.data[i].caption && media.data[i].caption.text) {
+          text = media.data[i].caption.text;
+          tags = media.data[i].caption.text.match((/#\w+/gi));
+          mentions = media.data[i].caption.text.match((/@\w+/gi));
+        }
+
+        var line_item = $('<li></li>');
+        var div = $('<div></div>');
+
+        $('<span>' + "hej" + "</span>", {"class": "badge"}).appendTo(div);
+        $('<img/>', {
+          "class": "img-responsive img-rounded"
+          "title": text,
+          "src": media.data[0].images.low_resolution.url
+        }).appendTo(div).appendTo(line_item).appendTo("#pings");
+      }
+    } else {
+      console.log("fail", event);
+    }
   };
 });
 
