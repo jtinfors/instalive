ae2953766aa9f1e7e082a895f2816d493ca542c1
Jens Tinfors <jens.tinfors@gmail.com>
2014-03-21 11:42:09 +0100

    adds debug

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ server.listen(app.get('port'), function(){
 
 var wss = new WebSocketServer({server: server});
 wss.on('connection', function(ws) {
-  console.log("connection! => ", ws);
+  console.log("connection! => ");
   wss.on('message', function(message) {
     var mess = JSON.parse(message);
     console.log("incoming message => ", mess);
