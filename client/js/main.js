dd4a81802e8e6468173393db5bfa1ad723e50b3b
Jens Tinfors <jens@tinfors.se>
2014-03-27 21:33:55 +0100

    Reafctor mustache templates to script tags

--- client/js/main.js
+++ client/js/main.js
@@ -64,62 +64,21 @@ function remove_some_items() {
 
 function display_alert(media) {
   console.log(media.message);
-  var alert = mustache.render("<div id=\"message\" class=\"alert alert-danger fade in\">\
-                                <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button>\
-                                <strong>{{heading}}</strong> {{message}}\
-                              </div>", media);
+  var alert = mustache.render(document.getElementById('mustache_alert').innerHTML, media);
   $(alert).prependTo("#content");
 }
 
 function handle_incoming_media(media) {
-    if(media.meta.code == 200 && media.data.length > 0) {
-      for(var i=0; i < media.data.length;i++) {
-        if(document.getElementById("image_"+media.data[i].id)) { return; }
-        /*jshint multistr: true */
-        var item = mustache.render("<li id=\"image_{{id}}\"><div class=\"row\">\
-                   <div class=\"image col-md-6 col-lg-6\">\
-                     <a href=\"{{link}}\" target=\"_blank\">\
-                     <img title=\"{{{caption.text}}}\"\
-                          src=\"{{{images.standard_resolution.url}}}\"\
-                          class=\"img-responsive img-rounded\"\
-                          height=\"{{images.standard_resolution.height}}\"\
-                          width=\"{{images.standard_resolution.width}}\"/>\
-                     </a>\
-                   </div>\
-                   <div class=\"meta col-md-6 col-lg-6\">\
-                     <div class=\"media\">\
-                       <a href=\"http://instagram.com/{{user.username}}\" class=\"pull-left\" target=\"_blank\">\
-                         <img class=\"media-object img-circle\" src=\"{{user.profile_picture}}\"/>\
-                       </a>\
-                       <div class=\"media-body\">\
-                          <a href=\"http://instagram.com/{{user.username}}\" class=\"pull-left\" target=\"_blank\">\
-                           <h4 class=\"media-heading\">{{user.full_name}}</h4>\
-                          </a>\
-                       </div>\
-                     {{#caption}}\
-                       {{#text}}\
-                         <p>{{caption.text}}</p>\
-                       {{/text}}\
-                     {{/caption}}\
-                     </div>\
-                     {{#tags}}\
-                       <span title=\"Tag\" class=\"label label-default\">{{.}}</span>\
-                     {{/tags}}\
-                     {{#filter}}\
-                       <span title=\"Filter\" class=\"label label-info\"><span class=\"glyphicon glyphicon-tint\"></span> {{.}}</span>\
-                     {{/filter}}\
-                     {{#location}}\
-                       {{#name}}\
-                         <span title=\"Location\" class=\"label label-success\"><span class=\"glyphicon glyphicon-cloud-upload\"></span> {{.}}</span>\
-                       {{/name}}\
-                     {{/location}}\
-                     {{#datetime}}\
-                       <span class=\"label label-primary\"><span class=\"glyphicon glyphicon-time\"></span> <date datetime=\"{{datetime}}\">{{relative_time}}</date></span>\
-                     {{/datetime}}\
-                   </div>\
-                 </div></li>", util.parse_date(util.strip_tags(media.data[i])));
-        $(item).prependTo("#pings");
-      }
+  if(media.meta.code == 200 && media.data.length > 0) {
+    for(var i=0; i < media.data.length;i++) {
+      if(document.getElementById("image_"+media.data[i].id)) { return; }
+
+      var item = mustache.render(
+        document.getElementById('mustache_post').innerHTML,
+        util.parse_date(util.strip_tags(media.data[i]))
+      );
+      $(item).prependTo("#pings");
     }
+  }
 }
 
