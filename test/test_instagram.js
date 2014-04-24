e1c4d83833ef3ede4d8041048c0b9a8e1ccb5249
Jens Tinfors <jens.tinfors@gmail.com>
2014-04-24 19:49:50 +0200

    Refactor instagram to lib and make it return obj

--- test/test_instagram.js
+++ test/test_instagram.js
@@ -1,63 +1,37 @@
 var assert = require("assert"),
-    fs = require('fs'),
     _ = require('underscore'),
-    instagram = require('../instagram');
+    querystring             = require('querystring'),
+    Instagram = new require('../lib/instagram');
 
 describe('instagram', function() {
   describe('#generate_post_data', function() {
     it('should return -1 when no location is given', function() {
-      assert.equal(-1, instagram.generate_post_data({}));
+      assert.equal(-1, Instagram.generate_post_data({}));
     });
 
     it('should provide correct lat/lng for sthlm', function() {
-      var data = instagram.generate_post_data({location: 'sthlm'});
+      var data = Instagram.generate_post_data({location: 'sthlm'});
       assert.equal(data.lat, '59.32536');
       assert.equal(data.lng, '18.071197');
-      assert.equal(data.radius, 4000)
+      assert.equal(data.radius, 4000);
     });
 
     it('should return negative when location is unknown', function() {
-      assert.equal(-1, instagram.generate_post_data('örebro'));
+      assert.equal(-1, Instagram.generate_post_data('örebro'));
     });
 
     it('should handle both default and specified value for radius', function() {
-      assert.equal(instagram.generate_post_data({location: 'sthlm', radius: 3000}).radius, 3000);
-      assert.equal(instagram.generate_post_data({location: 'sthlm'}).radius, 4000);
+      assert.equal(Instagram.generate_post_data({location: 'sthlm', radius: 3000}).radius, 3000);
+      assert.equal(Instagram.generate_post_data({location: 'sthlm'}).radius, 4000);
     });
   });
 
   describe('#generate_subscriptions_options', function() {
     it('should generate non-nil Content-Length', function() {
-      var data = instagram.urlencode(instagram.generate_post_data({location: 'sthlm'}));
-      var options = instagram.generate_subscriptions_options(Buffer.byteLength(data));
+      var data = querystring.stringify(Instagram.generate_post_data({location: 'sthlm'}));
+      var options = Instagram.generate_subscriptions_options(Buffer.byteLength(data));
       assert.ok(options.headers['Content-Length'] > 0);
     });
   });
 
-  describe('#subscribe', function() {
-    it('should fail when location is unknown', function() {
-      instagram.subscribe("pennybridge", function(err, data) {
-        assert.ok(err);
-      });
-    });
-  });
-
-  describe('#subscriptions', function() {
-    it('should return data', function(done) {
-      instagram.subscriptions(function(err, data) {
-        assert.ok(JSON.parse(data).data != null);
-        done();
-      });
-    });
-  });
-
-  describe('#fetch_new_geo_media', function() {
-    it('should return proper data', function(done) {
-      instagram.fetch_new_geo_media(123, 1, function(err, data) {
-        var result = JSON.parse(data);
-        assert.ok(result.meta.code == 200);
-        done();
-      })
-    });
-  });
 });
