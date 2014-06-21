98e4cc389ea6992e1ddbbf440a10e96b7214522a
Jens Tinfors <jens.tinfors@gmail.com>
2014-06-21 20:36:28 +0200

    yadda

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,17 @@
 $(function() {
+
+	/*
+	 *if (docCookies.hasItem('location')) {
+	 *  window.location = docCookies.getItem('location');
+	 *}
+	 */
+
 	$("select[name='locations']").selectpicker({style: 'btn-primary', menuStyle: 'dropdown-inverse'});
 
 	$("select[name='locations']").change(function(event) {
-		console.log($(event.target).parent().attr('action'));
-		window.location = $(event.target).parent().attr('action') + '/' + $("select[name='locations'] option:selected").val();
+		var location = $(event.target).parent().attr('action') + '/' + $("select[name='locations'] option:selected").val();
+		docCookies.setItem('location', location, 2147483647, '/');
+		window.location = location;
 	});
+
 });
