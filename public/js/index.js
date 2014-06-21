4684028ed8127dca10be41900961ea1603d3206f
Jens Tinfors <jens.tinfors@gmail.com>
2014-06-21 19:23:34 +0200

    adds navigation

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,8 @@
 $(function() {
 	$("select[name='locations']").selectpicker({style: 'btn-primary', menuStyle: 'dropdown-inverse'});
+
+	$("select[name='locations']").change(function(event) {
+		console.log($(event.target).parent().attr('action'));
+		window.location = $(event.target).parent().attr('action') + '/' + $("select[name='locations'] option:selected").val();
+	});
 });
