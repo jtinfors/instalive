8e7a9c7caddc34077ca66d8df04a20ca33234810
Jens Tinfors <jens.tinfors@gmail.com>
2015-02-28 16:40:34 +0100

    yadda

--- client/js/stores/InstagramStore.js
+++ client/js/stores/InstagramStore.js
@@ -20,7 +20,7 @@ function update(updates) {
 	 *});
 	 */
 
-	var newItems updates.filter(function(newItem) {
+	var newItems = updates.filter(function(newItem) {
 		return _instagrams.some(function(existingItem) {
 			return newItem.id === existingItem.id;
 		});
